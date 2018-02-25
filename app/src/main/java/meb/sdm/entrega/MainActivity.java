85dcae515d3ce48a815b18a83fee5d06a93c6182
braxanmp <braxanmp@gmail.com>
2018-02-25 13:03:31 +0100

    Main finalizado, falta el botón de credits en el action bar

--- app/src/main/java/meb/sdm/entrega/MainActivity.java
+++ app/src/main/java/meb/sdm/entrega/MainActivity.java
@@ -1,7 +1,10 @@
 package meb.sdm.entrega;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonPushed(View view) {
+        Log.d("push", "some button pushed");
+        if(view.equals(findViewById(R.id.image_gamepad)) || view.equals(findViewById(R.id.label_gamepad)) ){
+            Intent intent = new Intent(this, PlayActivity.class);
+            startActivity(intent);
+
+        }else if(view.equals(findViewById(R.id.image_score)) || view.equals(findViewById(R.id.label_score))){
+            Intent intent = new Intent(this, ScoreActivity.class);
+            startActivity(intent);
+        }
+        else if(view.equals(findViewById(R.id.image_settings)) || view.equals(findViewById(R.id.label_settings))){
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+
 }
