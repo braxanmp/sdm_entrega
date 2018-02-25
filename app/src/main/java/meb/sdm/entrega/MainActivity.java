7723405df7b8f1ebac85de6821c2aea67e8c2f32
braxanmp <braxanmp@gmail.com>
2018-02-25 18:55:18 +0100

    Finalizando CreditsActivity

--- app/src/main/java/meb/sdm/entrega/MainActivity.java
+++ app/src/main/java/meb/sdm/entrega/MainActivity.java
@@ -1,9 +1,11 @@
 package meb.sdm.entrega;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +16,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        menu.findItem(R.id.item_info).setVisible(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menu){
+        switch (menu.getItemId()) {
+            case R.id.item_info:
+                Intent intent = new Intent(this, CreditsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return false;
+        }
+    }
+
     public void buttonPushed(View view) {
-        Log.d("push", "some button pushed");
         if(view.equals(findViewById(R.id.image_gamepad)) || view.equals(findViewById(R.id.label_gamepad)) ){
             Intent intent = new Intent(this, PlayActivity.class);
             startActivity(intent);
